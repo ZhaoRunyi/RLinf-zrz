10cf8b7c4afb0527f6a8f44597b47891584fc464
Andy Lin <32576375+andylin-hao@users.noreply.github.com>
2025-12-05 09:16:36 +0800

    feat: unify install script and docker (#384)
    Signed-off-by: Hao Lin <linhaomails@gmail.com>

--- rlinf/envs/env_manager.py
+++ rlinf/envs/env_manager.py
@@ -211,17 +211,9 @@ class EnvManager:
         env_cls = env_eval_cls if is_eval else env_train_cls
 
         if enable_offload:
-            import importlib
+            from rlinf.envs.offload_wrapper import get_offload_env
 
-            class_name = env_cls.__name__
-            offload_module = importlib.import_module("rlinf.envs.offload_wrapper")
-            if hasattr(offload_module, class_name):
-                offload_env_cls = getattr(offload_module, class_name)
-                self.env_cls = offload_env_cls
-            else:
-                raise RuntimeError(
-                    f"Environment class {class_name} does not support offload"
-                )
+            self.env_cls = get_offload_env(env_type)
             self.env = None
         else:
             self.env_cls = env_cls
